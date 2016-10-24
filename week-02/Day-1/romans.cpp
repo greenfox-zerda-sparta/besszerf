4a5d55b28830c3da08a7b0c9b4022079c550312d
besszerf <caruzzo74@gmail.com>
2016-10-24 22:07:00 +0200

    Roman numbers ready

--- week-02/Day-1/romans.cpp
+++ week-02/Day-1/romans.cpp
@@ -5,50 +5,95 @@ using namespace std;
 
 int main() {
 
+	cout << "Hi! Let's see the roman numbers!" << endl << endl;
 	int input;
-
-
-/*	I 		1-esek 1-3  + I
-			4-es  5 ele I
-			5-o:s: L
-			6-8 L + I
-			9 10-es -I
-
-	II 		10 -es 1-3 +X
-			50		L
-
-	III		100 -as   C
-
-	IV		1000 -es M
-*/
 	do {
 		string out = "Roman number: ";
-		cout << "Enter a number between 1 and 3999 (or press 'x' for exit): ";
+		cout << "Enter a number between 1 and 3999 (or -1 for exit): ";
 		cin >> input;
 
-		if (input < 1 && input > 3999){
-			cout << "Your input is illegal" << endl;
+		if (input == -1){
+			break;
+		} else if (input < 1 || input > 3999){
+			cout << "Your input is invalid" << endl;
 			continue;
 		}
-		int ones = input % 10;
-		int tens = (input - ones)%100;
-		int hundreds = (input - ones - tens)%1000;
-		int tous =  (input - ones - tens - hundreds)%10000;
+
+		int tous = 0, hundreds = 0, tens = 0;
+		if (input >= 1000) {
+			tous = input / 1000;
+			input = input % (tous * 1000);
+		}
+		if (input >= 100) {
+			hundreds = input / 100;
+			input = input % (hundreds * 100);
+		}
+		if (input >= 10) {
+			tens = input / 10;
+			input = input % (tens * 10);
+		}
+		int ones = input;
+
 
 		if (tous > 0) {
-			for (short i = 1; i < tous; i++){
+			for (short i = 0; i < tous; i++){
 				out = out + "M";
 			}
 		}
 		if (hundreds > 0 && hundreds < 4){
-			for (short i = 1; i < hundreds; i++){
+			for (short i = 0; i < hundreds; i++){
 				out = out + "C";
 			}
+		} else if (hundreds == 4){
+			out = out + "CD";
+		} else if (hundreds == 5){
+			out = out + "D";
+		} else if (hundreds > 5 && hundreds < 9){
+			out = out + "D";
+			for (short i = 0; i < hundreds -5; i++ ){
+				out = out + "C";
+			}
+		} else if (hundreds == 9){
+			out = out + "CM";
+		}
+		if (tens > 0 && tens < 4){
+			for (short i = 0; i < tens; i++){
+				out = out + "X";
+			}
+		} else if (tens == 4){
+			out = out + "XL";
+		} else if (tens == 5){
+			out = out + "L";
+		} else if (tens > 5 && tens < 9){
+			out = out + "L";
+			for (short i = 0; i < tens -5; i++ ){
+				out = out + "X";
+			}
+		} else if (tens == 9){
+			out = out + "XC";
+		}
+		if (ones > 0 && ones < 4){
+			for (short i = 0; i < ones; i++){
+				out = out + "I";
+			}
+		} else if (ones == 4){
+			out = out + "IV";
+		} else if (ones == 5){
+			out = out + "V";
+		} else if (ones > 5 && ones < 9){
+			out = out + "V";
+			for (short i = 0; i < ones -5; i++ ){
+				out = out + "I";
+			}
+		} else if (ones == 9){
+			out = out + "IX";
+		}
+		if (input != -1) {
+			cout << out << endl;
 		}
 
-	} while (input != 'x');
+	} while (input != -1);
 
 	cout << "Bye!" << endl;
-
 	return 0;
 }
