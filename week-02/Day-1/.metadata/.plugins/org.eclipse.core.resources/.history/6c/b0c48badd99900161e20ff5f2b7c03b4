d8a4f2be15dd142ba0b73747b9d00099f735014b
besszerf <caruzzo74@gmail.com>
2016-10-24 14:05:46 +0200

    Booleans exercises done

---
+++ week-02/Day-1/.metadata/.plugins/org.eclipse.core.resources/.history/6c/b0c48badd99900161e20ff5f2b7c03b4
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int f = 16;
+	// please square root f's value
+	f = sqrt(f);
+	cout << "f = " << f << endl;
+	return 0;
+}
---
+++ week-02/Day-1/ex8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int h1 = 350;
+	int h2 = 200;
+	// tell if the double of h2 is bigger than h1
+	if (h2*2>h1)
+		cout << "h2*2 is bigger than h1";
+	return 0;
+}
