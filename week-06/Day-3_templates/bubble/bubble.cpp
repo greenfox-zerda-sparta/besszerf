0026142ef4fa773f4773c91d849f796498cdf1dd
besszerf <caruzzo74@gmail.com>
2016-12-01 14:45:46 +0100

    Bubbles <feat>bubble speed vector is randomly set.

--- week-06/Day-3_templates/bubble/bubble.cpp
+++ week-06/Day-3_templates/bubble/bubble.cpp
@@ -26,4 +26,7 @@ Bubble::Bubble() {
   position.set_x(rand() %10);
   position.set_y(rand() %10);
   std::cout << "Random Position of bubble (x, y) = ("<< position.get_x() << ", " << position.get_y() << ")"<< std::endl;
+  speed_vector.set_x(rand() %10);
+  speed_vector.set_y(rand() %10);
+  std::cout << "   Speed vector (x, y) = ("<< position.get_x() << ", " << position.get_y() << ")"<< std::endl;
 }
