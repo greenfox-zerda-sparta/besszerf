f754fc53555ad005ab940026e3c110a5d8efff33
besszerf <caruzzo74@gmail.com>
2016-12-01 11:12:47 +0100

    Bubbles <feat>game board class base is ready.
    - print board is functioning
    - board.hpp is added

---
+++ week-06/Day-3_templates/bubble/board.hpp
@@ -0,0 +1,51 @@
+/*
+ * board.h
+ *
+ *  Created on: 2016.12.01.
+ *      Author: caruzzo74
+ */
+
+#ifndef BOARD_HPP_
+#define BOARD_HPP_
+#include <string>
+#include <vector>
+#include <iostream>
+
+typedef unsigned int uint;
+typedef std::vector<std::string> _row;
+typedef std::vector<_row> _board;
+
+template<class T>
+class Board {
+  private:
+    _board game_map;
+    static uint game_map_columns;
+    static uint game_map_rows;
+  public:
+    Board();
+    void print_board();
+
+};
+
+template<class T>
+uint Board<T>::game_map_rows = 10;
+
+template<class T>
+uint Board<T>::game_map_columns = 10;
+
+template<class T>
+Board<T>::Board() {
+    game_map = _board(game_map_rows, _row(game_map_columns, "0"));
+}
+
+template<class T>
+void Board<T>::print_board() {
+  for (uint row = 0; row < game_map_rows; row++) {
+    for (uint column = 0; column < game_map_columns; column++) {
+      std::cout << game_map[row][column];
+    }
+    std::cout << std::endl;
+  }
+}
+
+#endif /* BOARD_HPP_ */
