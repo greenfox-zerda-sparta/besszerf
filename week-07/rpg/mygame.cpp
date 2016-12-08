07cc9ec2a6de1e2cc2abbfa03069d983f2a3f256
besszerf <caruzzo74@gmail.com>
2016-12-08 09:19:57 +0100

    RPG<feat>Badboys are on the board in random position

--- week-07/rpg/mygame.cpp
+++ week-07/rpg/mygame.cpp
@@ -12,6 +12,8 @@ void MyGame::init(GameContext& context) {
   context.load_file("pics/hero-up.bmp");
   context.load_file("pics/hero-left.bmp");
   context.load_file("pics/hero-right.bmp");
+  context.load_file("pics/skeleton.bmp");
+  context.load_file("pics/boss.bmp");
 }
 
 void MyGame::render(GameContext& context) {
@@ -22,7 +24,14 @@ void MyGame::render(GameContext& context) {
       context.draw_sprite((myBoard[i][j]?"pics/floor.bmp":"pics/wall.bmp"), x, y);
     }
   }
-  context.draw_sprite(hero_pic(context), hero_position.get_x(), hero_position.get_y());
+  for (int i = 0; i < bad_boys_count; i++) {
+    if (i == 0) {
+      context.draw_sprite("pics/boss.bmp", bad_boys_positions[i].get_x(), bad_boys_positions[i].get_y());
+    } else {
+      context.draw_sprite("pics/skeleton.bmp", bad_boys_positions[i].get_x(), bad_boys_positions[i].get_y());
+    }
+  }
+  context.draw_sprite(hero_pic, hero_position.get_x(), hero_position.get_y());
   hero_move(context);
   context.render();
 }
@@ -30,14 +39,12 @@ void MyGame::render(GameContext& context) {
 MyGame::MyGame() {
   myBoard = board(board_rows, row(board_cols, 0));
   drawLevel(0, 0, 0);
-  for (uint i = 0; i < myBoard.size(); i++) {
-    for (uint j = 0; j < myBoard[i].size(); j++) {
-      if (myBoard[i][j]) {
-        hero_position.set_x(i * tile_width);
-        hero_position.set_y(j * tile_height);
-        return;
-      }
-    }
+  hero_pic = "pics/hero-down.bmp";
+  set_hero_initial_position();
+  bad_boys_count = 4;
+  bad_boys_positions = vector<Point2d>(bad_boys_count);
+  for (int i = 0; i < bad_boys_count; i++) {
+    bad_boys_positions[i] = set_badboy_position();
   }
 }
 
@@ -96,7 +103,45 @@ void MyGame::drawLevel(int x, int y, int covered){
     }
   }
   drawLevel(x, y, covered);
-  return; //for safety, shoud never reach this line anyways...
+  return;
+}
+
+void MyGame::set_hero_initial_position() {
+  for (uint i = 0; i < myBoard.size(); i++) {
+    for (uint j = 0; j < myBoard[i].size(); j++) {
+      if (myBoard[i][j]) {
+        hero_position.set_x(i * tile_width);
+        hero_position.set_y(j * tile_height);
+        return;
+      }
+    }
+  }
+}
+
+Point2d MyGame::set_badboy_position() {
+  Point2d position;
+  do {
+    position.set_x((rand() % board_cols) * tile_width);
+    position.set_y((rand() % board_rows) * tile_height);
+  } while (!is_a_good_position(position));
+  return position;
+}
+
+bool MyGame::is_a_good_position(Point2d position) {
+  int x = position.get_x()/tile_width;
+  int y = position.get_y()/tile_width;
+  if (!myBoard[x][y]) {
+    return false;
+  }
+  for (int i = 0; i < bad_boys_count; i++) {
+    if (bad_boys_positions[i] == position) {
+      return false;
+    }
+  }
+  if (hero_position == position) {
+    return false;
+  }
+  return true;
 }
 
 void MyGame::print_board() {
@@ -109,40 +154,31 @@ void MyGame::print_board() {
   std::cout << std::endl;
 }
 
-std::string MyGame::hero_pic(GameContext& context) {
-  if (context.was_key_pressed(ARROW_UP)) {
-    return "pics/hero-up.bmp";
-  }
-  if (context.was_key_pressed(ARROW_LEFT)) {
-    return "pics/hero-left.bmp";
-  }
-  if (context.was_key_pressed(ARROW_RIGHT)) {
-    return "pics/hero-right.bmp";
-  }
-  return "pics/hero-down.bmp";
-}
-
 void MyGame::hero_move(GameContext& context) {
   int x = hero_position.get_x()/tile_width;
   int y = hero_position.get_y()/tile_height;
   if (context.was_key_pressed(ARROW_DOWN)) {
     if (y < board_rows - 1 && myBoard[x][y + 1]) {
       hero_position.set_y(hero_position.get_y() + tile_height);
+      hero_pic = "pics/hero-down.bmp";
     }
   }
   if (context.was_key_pressed(ARROW_UP)) {
     if (y > 0 && myBoard[x][y - 1]) {
       hero_position.set_y(hero_position.get_y() - tile_height);
+      hero_pic = "pics/hero-up.bmp";
     }
   }
   if (context.was_key_pressed(ARROW_LEFT)) {
     if (x > 0 && myBoard[x - 1][y]) {
       hero_position.set_x(hero_position.get_x() - tile_width);
+      hero_pic = "pics/hero-left.bmp";
     }
   }
   if (context.was_key_pressed(ARROW_RIGHT)) {
     if (x < board_cols - 1 && myBoard[x + 1][y]) {
       hero_position.set_x(hero_position.get_x() + tile_width);
+      hero_pic = "pics/hero-right.bmp";
     }
   }
 }
--- week-07/rpg/mygame.h
+++ week-07/rpg/mygame.h
@@ -8,7 +8,7 @@
 typedef std::vector<bool> row;
 typedef std::vector<row> board;
 
-const int COVERABLE = int (0.54 * board_rows * board_cols);
+const int COVERABLE = int (0.62 * board_rows * board_cols);
 
 class MyGame : public Game {
   public:
@@ -19,10 +19,16 @@ class MyGame : public Game {
   private:
     board myBoard;
     Point2d hero_position;
-    std::string hero_pic(GameContext& context);
+    std::vector<Point2d> bad_boys_positions;
+    int bad_boys_count;
+    std::string hero_pic;
+    Point2d set_badboy_position();
     void drawLevel(int x, int y, int covered);
     void print_board();
     void hero_move(GameContext& context);
+    void set_hero_initial_position();
+    Point2d generate_random_position();
+    bool is_a_good_position(Point2d position);
 };
 
 #endif /* MYGAME_H_ */
