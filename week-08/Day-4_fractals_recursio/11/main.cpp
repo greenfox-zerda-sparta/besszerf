4f2baa0aa622358024d75dd0485bf2fff624d3cd
besszerf <caruzzo74@gmail.com>
2016-12-16 09:05:58 +0100

    Exercise 11 is recursive, but it is not perfect

--- week-08/Day-4_fractals_recursio/11/main.cpp
+++ week-08/Day-4_fractals_recursio/11/main.cpp
@@ -7,22 +7,35 @@ using namespace std;
 
 class fractal_drawer {
   public:
-    fractal_drawer(SDL_Renderer* renderer);
+    fractal_drawer();
     void draw_rectangle();
-    void draw_fractal();
+    void draw_fractal(int x);
+    void set_renderer(SDL_Renderer* renderer);
   private:
     SDL_Renderer* renderer;
     draw d;
     pair<int, int> position;
+    int phase;
+    pair<int, int> original_position;
+    pair<int, int> phase1_position;
+    pair<int, int> phase2_position;
+    pair<int, int> phase3_position;
+    pair<int, int> phase4_position;
     int offset;
 
 
 };
-fractal_drawer::fractal_drawer(SDL_Renderer* renderer) {
+fractal_drawer::fractal_drawer() {
+  phase = 1;
   offset = 600;
+  original_position = make_pair(1, 1);
+  position = original_position;
+  renderer = NULL;
+//  draw_rectangle();
+}
+
+void fractal_drawer::set_renderer(SDL_Renderer* renderer) {
   this->renderer = renderer;
-  position = make_pair(1, 1);
-  draw_rectangle();
 }
 
 void fractal_drawer::draw_rectangle () {
@@ -34,41 +47,47 @@ void fractal_drawer::draw_rectangle () {
 
 }
 
-void fractal_drawer::draw_fractal() {
-  offset /= 3;
-  position.first = 1;
+void fractal_drawer::draw_fractal(int x) {
   draw_rectangle();
-  position.first += offset;
-  draw_rectangle();
-  position.first += offset;
-  draw_rectangle();
-  if (position.first == 1+offset*2) {
-    position.first = 1;
+  if (x == 0) {
+    offset = 600;
+    position.first = original_position.first;
+    position.second = original_position.second;
+    draw_rectangle();
+    return;
   }
-  position.second += offset;
-  draw_rectangle();
-  position.first += offset;
-  draw_rectangle();
-  position.first += offset;
-  draw_rectangle();
-  if (position.first == 1+offset*2) {
-    position.first = 1;
+  if (phase == 1) {
+    offset /= 3;
+    position.first = original_position.first + offset;
+    position.second = original_position.second;
+    ++phase;
+  } else if (phase == 2) {
+    position.first = original_position.first;
+    position.second = original_position.second + offset;
+    ++phase;
+  } else if (phase == 3) {
+    position.first = original_position.first + 2*offset;
+    position.second = original_position.second + offset;
+    ++phase;
+  } else if (phase == 4) {
+    position.first =  original_position.first + offset;
+    position.second = original_position.second + 2*offset;
+    phase = 1;
+    x -= 1;
   }
-  position.second += offset;
-  draw_rectangle();
-  position.first += offset;
-  draw_rectangle();
-  position.first += offset;
-  draw_rectangle();
+  draw_fractal(x);
 }
 
-void create_window(int red, int green, int blue) {
+void create_window(fractal_drawer drawer, int red, int green, int blue) {
   bool quit = false;
   SDL_Event event;
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window* window = SDL_CreateWindow("SDL2 Fractal",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 603, 603, 0);
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+  /*********************************************/
+  drawer.set_renderer(renderer);
+  /*********************************************/
   SDL_SetRenderDrawColor(renderer, red, green, blue, 0);
   while (!quit) {
     SDL_WaitEvent(&event);
@@ -81,8 +100,7 @@ void create_window(int red, int green, int blue) {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     /*********************************************/
-    fractal_drawer drawer(renderer);
-    drawer.draw_fractal();
+    drawer.draw_fractal(6);
     /*********************************************/
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderPresent(renderer);
@@ -93,6 +111,7 @@ void create_window(int red, int green, int blue) {
  }
 
 int main(int argc, char ** argv) {
-  create_window(255, 255, 170);
+  fractal_drawer drawer;
+  create_window(drawer, 255, 255, 170);
  return 0;
 }
